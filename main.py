6889ca3e89b89f660388078d487e4b99413615f4
EdH681 <edwardhornby07@gmail.com>
2024-02-23 18:47:54 +0000

    finished physics and sprites

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
-import sys
 from math import sqrt, fabs
+import time
+
+
+def timer(func):
+    start = time.time()
+    func()
+    end = time.time()
+    print(end-start)
 
 
 class Obstacle:
@@ -13,12 +20,12 @@ class Obstacle:
 
     def hitbox(self):
         return {"top": self.y, "bottom": self.y + self.sprite.get_height(),
-                "left": self.x, "right": self.x + (self.sprite.get_width()*self.reps)}
+                "left": self.x, "right": self.x + (self.sprite.get_width() * self.reps)}
 
     def display(self):
         sprite_width = self.sprite.get_width()
         for i in range(self.reps):
-            self._display.blit(self.sprite, (self.x+(i*sprite_width), self.y))
+            self._display.blit(self.sprite, (self.x + (i * sprite_width), self.y))
 
     def safe(self):
         return self._safe
@@ -35,10 +42,11 @@ class SpikedPlatform(Obstacle):
             self._display.blit(self.sprite, (self.x + (i * sprite_width), self.y))
 
         spike_number = (self.reps * self.sprite.get_width()) // 10
-        spike_start = self.x + ((self.reps*self.sprite.get_width()) - (spike_number * 10))/2
+        spike_start = self.x + ((self.reps * self.sprite.get_width()) - (spike_number * 10)) / 2
 
         for i in range(spike_number):
-            points = ((spike_start+(i*10), self.y), (spike_start+(i*10)+10, self.y), (spike_start+(i*10)+5, self.y-5))
+            points = ((spike_start + (i * 10), self.y), (spike_start + (i * 10) + 10, self.y),
+                      (spike_start + (i * 10) + 5, self.y - 5))
             pygame.draw.polygon(self._display, "black", points)
 
 
@@ -51,7 +59,29 @@ class Wall(Obstacle):
     def display(self):
         sprite_height = self.sprite.get_height()
         for i in range(self.reps):
-            self._display.blit(self.sprite, (self.x, self.y+(sprite_height*i)))
+            self._display.blit(self.sprite, (self.x, self.y + (sprite_height * i)))
+
+    def hitbox(self):
+        return {"top": self.y, "bottom": self.y + (self.sprite.get_height() * self.reps),
+                "left": self.x, "right": self.x + self.sprite.get_width()}
+
+
+class SpikedWall(Wall):
+    def __init__(self, display, pos, repetitions):
+        super().__init__(display, pos, repetitions)
+        self._safe = False
+
+    def display(self):
+        sprite_height = self.sprite.get_height()
+        for i in range(self.reps):
+            self._display.blit(self.sprite, (self.x, self.y + (sprite_height * i)))
+
+        spike_number = (self.reps * self.sprite.get_height()) // 10
+        spike_start = self.y - ((self.reps * self.sprite.get_height()) - (spike_number * 10)) / 2
+        for i in range(spike_number):
+            points = ((self.x, spike_start + (i * 10)), (self.x, spike_start + (i * 10) + 10),
+                      (self.x - 5, spike_start + (i * 10) + 5))
+            pygame.draw.polygon(self._display, "black", points)
 
 
 class Player:
@@ -99,6 +129,9 @@ class Player:
 
         if self.rect.bottom > max_height:
             self.rect.y = max_height - sprite_height
+            self.landed = True
+        else:
+            self.landed = False
 
         if self.rect.top < 0:
             self.rect.y = 0
@@ -112,7 +145,7 @@ class Player:
             self.rect.x = self.display.get_width() - self.rect.width
             self.x_vel = 0
 
-        for plt in platforms:
+        for plt in self.platforms:
             if plt.hitbox()["left"] < self.rect.centerx < plt.hitbox()["right"]:
 
                 if plt.hitbox()["top"] < self.rect.bottom < plt.hitbox()["bottom"]:
@@ -127,18 +160,25 @@ class Player:
                     self.rect.y = plt.hitbox()["bottom"]
                     self.y_vel = 0
 
-                if plt.hitbox()["top"] < self.rect.centery < plt.hitbox()["bottom"]:
-                    if plt.hitbox()["left"] < self.rect.left < plt.hitbox()["right"]:
-                        self.rect.x = plt.hitbox()["right"]
-                        self.x_vel = 0
-                    if plt.hitbox()["left"] < self.rect.right < plt.hitbox()["right"]:
-                        self.rect.x = plt.hitbox()["left"] - self.rect.width
-                        self.x_vel = 0
+            if plt.hitbox()["top"] < self.rect.centery < plt.hitbox()["bottom"]:
+                if plt.hitbox()["left"] < self.rect.left < plt.hitbox()["right"]:
+                    self.rect.x = plt.hitbox()["right"]
+                    self.x_vel = 0
+                    if not plt.safe():
+                        self.__die()
+                if plt.hitbox()["left"] < self.rect.right < plt.hitbox()["right"]:
+                    self.rect.x = plt.hitbox()["left"] - self.rect.width
+                    self.x_vel = 0
+                    if not plt.safe():
+                        self.__die()
 
     def __x_component(self):
         friction = self.FRICTION
         if self.landed:
             friction = self.PLATFORM_FRICTION
+
+        if fabs(self.x_vel) <= 0.1:
+            self.x_vel = 0
 
         if self.x_ready and pygame.mouse.get_pressed()[0]:
             self.x_ready = False
@@ -152,6 +192,7 @@ class Player:
 
     def __die(self):
         pygame.time.delay(100)
+        self.x_vel, self.y_vel = 0, 0
         self.rect.x = 50
         self.rect.y = 500
 
@@ -175,7 +216,6 @@ class Player:
         unit_vector_y = self.cursor_distance[1] / self.magnitude
         endpoint_x = self.rect.x + (unit_vector_x * self.CLICK_FORCE * 10)
         endpoint_y = self.rect.y - (unit_vector_y * self.CLICK_FORCE * 10)
-        print(endpoint_y)
         pygame.draw.line(self.display, "red", (self.rect.x, self.rect.y), (endpoint_x, endpoint_y))
 
     def run(self):
@@ -186,36 +226,39 @@ class Player:
         self.__display()
 
 
-size = window_width, window_height = 1500, 750
-pygame.init()
-win = pygame.display.set_mode(size)
-clock = pygame.time.Clock()
+if __name__ == "__main__":
+    size = window_width, window_height = 1500, 750
+    pygame.init()
+    win = pygame.display.set_mode(size)
+    player_display = pygame.Surface(size)
+    clock = pygame.time.Clock()
 
-start = Obstacle(win, (0, 600), 3)
-bad = SpikedPlatform(win, (100, 300), 4)
-wall = Wall(win, (900, 100), 10)
-platforms = [start, bad, wall]
+    objects = [
+        Obstacle(win, (0, 600), 10),
+        SpikedPlatform(win, (100, 300), 4),
+        Wall(win, (900, 100), 10),
+        SpikedWall(win, (1200, 200), 5)
+    ]
 
+    player = Player(win, objects)
 
+    # mainloop
+    running = True
+    bg_image = pygame.image.load("assets/backdrop.png").convert()
+    bg_image = pygame.transform.scale(bg_image, (window_width, window_height))
+    while running:
+        start = time.time()
+        win.blit(bg_image, (0, 0))
+        player.run()
+        for obj in objects:
+            obj.display()
+        pygame.display.update()
 
-player = Player(win, platforms)
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
 
-
-# mainloop
-running = True
-bg_image = pygame.image.load("assets/backdrop.png")
-bg_image = pygame.transform.scale(bg_image, (window_width, window_height))
-while running:
-
-    win.blit(bg_image, (0, 0))
-    player.run()
-    for platform in platforms:
-        platform.display()
-    wall.display()
-    pygame.display.update()
-
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
+        end = time.time()
+        print(end-start)
